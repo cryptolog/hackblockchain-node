3c3a583ff02fe1a20b478ca8d1ab0cf35b7021c8
Nav Saini <hi@navdeep.io>
2018-03-07 22:59:49 +0300

    add layout template

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 
+var express = require('express')
+var config = require('./config/config')
+var db = require('./app/models')
 
-var express = require('express'),
-  config = require('./config/config'),
-  db = require('./app/models');
+var app = express()
 
-var app = express();
-
-module.exports = require('./config/express')(app, config);
+module.exports = require('./config/express')(app, config)
 
 db.sequelize
   .sync()
   .then(function () {
     if (!module.parent) {
       app.listen(config.port, function () {
-        console.log('Express server listening on port ' + config.port);
-      });
+        console.log('Express server listening on port ' + config.port)
+      })
     }
   }).catch(function (e) {
-    throw new Error(e);
-  });
-
+    throw new Error(e)
+  })
