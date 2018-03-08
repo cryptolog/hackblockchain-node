9d0922b4389bda6f30b9427e9bbe9c375d15b0b3
Nav Saini <hi@navdeep.io>
2018-03-08 12:40:58 +0300

    static assets working

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,16 +1,11 @@
 var express = require('express')
 var router = express.Router()
-var db = require('../models')
+// var db = require('../models')
 
 module.exports = function (app) {
   app.use('/', router)
 }
 
 router.get('/', function (req, res, next) {
-  db.Article.findAll().then(function (articles) {
-    res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
-    })
-  })
+  res.render('home.nunjucks')
 })
