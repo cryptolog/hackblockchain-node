d7caf18bbb74daf20854e6ca9e953cb64892a8ab
Nav Saini <hi@navdeep.io>
2018-03-12 17:18:47 +0300

    add express-admin

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,11 +1,25 @@
 var express = require('express')
 var router = express.Router()
 // var db = require('../models')
+var User = require('../models').User
 
 module.exports = function (app) {
   app.use('/user', router)
 }
 
+/* routes here */
+
 router.get('/', (req, res, next) => {
   return res.render('index.nunjucks')
+})
+
+router.get('/signup', (req, res) => {
+  // send out the sign up page
+  return res.render('signup')
+})
+
+router.post('/signup', (req, res) => {
+  // create the user
+  // inputted data will be validated against the definitions already in place
+  res.send('user created')
 })
