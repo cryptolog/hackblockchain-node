72cba952b2109cb23b612d038f4ef4814fadd02d
Nav Saini <hi@navdeep.io>
2018-03-14 23:04:59 +0300

    attune app.js to mongo

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 
 var express = require('express')
 var config = require('./config/config')
-var db = require('./app/models')
+var glob = require('glob')
+var mongoose = require('mongoose')
 
+mongoose.connect(config.db)
+var db = mongoose.connection
+db.on('error', function () {
+  throw new Error('unable to connect to database at ' + config.db)
+})
+
+var models = glob.sync(config.root + '/app/models/*.js')
+models.forEach(function (model) {
+  require(model)
+})
 var app = express()
 
 module.exports = require('./config/express')(app, config)
 
-db.sequelize
-  .sync()
-  .then(function () {
-    if (!module.parent) {
-      app.listen(config.port, function () {
-        console.log('Express server listening on port ' + config.port)
-      })
-    }
-  }).catch(function (e) {
-    throw new Error(e)
-  })
+app.listen(config.port, function () {
+  console.log('Express server listening on port ' + config.port)
+})
