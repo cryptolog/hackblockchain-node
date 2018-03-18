9aaa67507a348748d7d7187444f211a9dd7957ad
Nav Saini <hi@navdeep.io>
2018-03-18 21:29:54 +0300

    add shortid fix

--- app/models/ad.js
+++ app/models/ad.js
@@ -2,14 +2,16 @@
 
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
+var shortid = require('shortid')
 
 var AdSchema = new Schema({
+  _id: {type: String, default: shortid.generate},
   title: {type: String, required: true},
   description: {type: String, required: true},
   jobType: {type: String, required: true},
   apply: {type: String, required: true},
   location: {type: String, required: true},
-  user_id: String
+  user_id: {type: Schema.Types.ObjectId, ref: 'User'}
 }, {
   timestamps: true
 })
