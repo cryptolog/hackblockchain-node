fea3b2f76c2c58a2fd84949b56123c8b61242cec
Nav Saini <hi@navdeep.io>
2018-03-24 13:46:26 +0300

    add validations to user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,53 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
+let {isEmail, isAlpha, isURL} = require('validator')
 
 var UserSchema = new Schema({
-  email: {type: String, required: true},
-  password: {type: String, required: true},
+  email: {type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return isEmail(v)
+      }
+    }
+  },
+  password: {type: String, required: true, min: 8},
   paid: {type: Boolean, required: true},
   quota: {type: Number, default: 1, min: 1},
-  companyName: String,
-  companyUrl: String,
+  first: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return isAlpha(v)
+      }
+    }
+  },
+  last: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return isAlpha(v)
+      }
+    }
+  },
+  companyName: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return isAlpha(v)
+      }
+    }
+  },
+  companyUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return isURL(v)
+      }
+    }
+  },
   companyLogo: String
 }, {
   timestamps: true
