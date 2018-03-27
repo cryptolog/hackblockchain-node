f5d2574c29636c36be98c954b2cccbf7d0d50b18
Nav Saini <hi@navdeep.io>
2018-03-27 15:13:55 +0300

    expand home controller

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
-var mongoose = require('mongoose')
-var Ad = mongoose.model('Ad')
+// var mongoose = require('mongoose')
+// var Ad = mongoose.model('Ad')
 
 module.exports = function (app) {
   app.use('/', router)
@@ -11,4 +11,20 @@ router.get('/', function (req, res, next) {
   // send latest jobs here
   // get the latest ads
   res.render('home')
+})
+
+router.get('/login', function (req, res, next) {
+  res.render('login')
+})
+
+router.post('/login', function (req, res, next) {
+  //  passport haiku here
+})
+
+router.get('/signup', function (req, res, next) {
+  res.render('signup')
+})
+
+router.post('/signup', function (req, res, next) {
+  // create a new user and a new ad
 })
