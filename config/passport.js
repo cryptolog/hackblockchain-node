5a8010c8bb0318a80e56111db474c6419b8b0162
Nav Saini <hi@navdeep.io>
2018-03-30 14:30:36 +0300

    begin converting user to account

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,8 @@
 var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
-var mongoose = require('mongoose')
+// var LocalStrategy = require('passport-local').Strategy
+// var mongoose = require('mongoose')
 // import model
-var User = mongoose.model('User')
+// var Account = mongoose.model('Account')
 
-passport.use(new LocalStrategy((username, password, done) => {
-  User.findOne({email: username}, (err, user) => {
-    if (err) return done(err)
-
-    if (!user) {
-      return done(null, false, {message: 'incorrect username'})
-    }
-
-    if (!user.validPassword(password)) {
-      return done(null, false, {message: 'Incorrect password'})
-    }
-
-    return done(null, user)
-  })
-}))
-
-passport.serializeUser(function (user, done) {
-  done(null, user.id)
-})
-
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user)
-  })
-})
 
 module.exports = passport
