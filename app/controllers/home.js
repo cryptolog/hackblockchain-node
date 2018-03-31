f6ebe8f69672138f5531ed0bc65173c70f92c161
Nav Saini <hi@navdeep.io>
2018-03-31 19:06:59 +0300

    commit before attempted rebase

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -29,9 +29,14 @@ router.get('/signup', function (req, res, next) {
 })
 
 router.post('/signup', function (req, res, next) {
-  // create a new user and a new ad
+  // validate the password here
+  let {email, password} = req.body
+  res.redirect('/signup', {failureFlash: true})
+  // create an account and redirect to success page
+  // didn't get an email, resend it?
 })
 
 router.get('/dashboard', function (req, res, next) {
   // do something here
+  res.render('dashboard')
 })
